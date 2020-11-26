568ab73264951fd1deb792edbe398c837d08b768
Aleksandr Morozov <aleksandrmorozov@Aleksandr-Morozov.local>
2020-11-26 09:08:24 -0500

    Added Order class, all classes together.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #include "Orden.h"
+void cargaProductos(Producto productosARR[], int &productCount);
+void muestraProductos(Producto productosARR[], int productCount);
+
 
 int main(void)
 {
-    Categoria cafe("cafe");
-    Producto americano("Americano", cafe, 34.50, 10, true, "00242214424");
-    cout << americano.getNombre() << " " << americano.getPrecio() << " " << americano.getCantidad() << " " << americano.getDisponibilidad() << endl;
-    cout << americano.getCategoria().getNombre() << endl;
+    Producto productoARR[20];
+    int cantidadProductos;
 
-
-    Fecha fecha(10, 12, 2020);
-
-    Orden orden1;
-    cout << orden1.getFecha().getDia() << endl;
+    cargaProductos(productoARR, cantidadProductos);
+    cout << productoARR[0].getNombre() << endl;
+    cout << productoARR[0].getPrecio() << endl;
+    cout << productoARR[0].getCantidad() << endl;
 
     return 0;
 };
+
+
+//funcion carga datos del archivo, almanece en arreglo indicado.
+void cargaProductos(Producto productosARR[], int &productCount)
+{
+    //definir variables
+    string nombreProducto;
+    string nombreCatergoria;
+    double precio;
+    int cantidad;
+    string SKU;
+
+    ifstream file;
+    file.open("Productos.txt");
+
+    productCount = 0;
+    while ( file >> nombreProducto >> nombreCatergoria >> precio >> cantidad >> SKU)
+    {
+        productosARR[productCount].setNombre(nombreProducto);
+        productosARR[productCount].setCategoria(nombreCatergoria);
+        productosARR[productCount].setPrecio(precio);
+        productosARR[productCount].setCantidad(cantidad);
+        productosARR[productCount].setSKU(SKU);
+        productCount++;
+    }
+
+    file.close();
+};
+
+void muestraProductos(Producto productosARR[], int productCount)
+{
+    for(int i=0; i < productCount; i++)
+    {
+        cout << productosARR[productCount].getPrecio() << endl;
+    }
+}
